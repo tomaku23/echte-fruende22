e2845804b243523d6da0346ccfc2f28129a29071
tomaku23 <301857971+tomaku23@users.noreply.github.com>
2026-07-23 11:48:54 +0200

    Update navigation links in header component

--- components/header.php
+++ components/header.php
@@ -26,15 +26,15 @@ $navigation = [
 
     ],
 
-    "./termine.php" => [
-
-        "title" => "Termine"
-
-    ],
-
     "./zugkoenig.php" => [
 
         "title" => "Zugkönig"
+
+    ],
+
+    "./termine.php" => [
+
+        "title" => "Termine"
 
     ]
 
@@ -45,80 +45,3 @@ $currentPage = basename(
     $_SERVER["PHP_SELF"]
 
 );
-
-?>
-
-<header
-    class="header"
-    data-header="root">
-
-    <div class="header-inner">
-
-        <a
-            class="header-logo"
-            data-header="logo"
-            href="./index.php"
-            aria-label="Zur Startseite">
-
-            <img
-                src="../images/logo.png"
-                alt="Echte Fründe '22">
-
-        </a>
-
-        <nav
-            class="header-navigation"
-            data-header="navigation"
-            aria-label="Hauptnavigation"
-            aria-hidden="false">
-
-            <ul>
-
-                <?php foreach (
-
-                    $navigation as $href => $item
-
-                ): ?>
-
-                    <li>
-
-                        <a
-                            href="<?= htmlspecialchars($href) ?>"
-                            class="<?= basename($href) === $currentPage ? "active" : "" ?>">
-
-                            <?= htmlspecialchars($item["title"]) ?>
-
-                        </a>
-
-                    </li>
-
-                <?php endforeach; ?>
-
-            </ul>
-
-        </nav>
-
-        <button
-            class="header-burger"
-            data-header="burger"
-            type="button"
-            aria-label="Navigation öffnen"
-            aria-expanded="false">
-
-            <span></span>
-
-            <span></span>
-
-            <span></span>
-
-        </button>
-
-    </div>
-
-    <div
-        class="header-overlay"
-        data-header="overlay">
-
-    </div>
-
-</header>
